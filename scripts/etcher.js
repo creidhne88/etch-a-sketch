a9a262088517df224c568c7552c868d637e8c5f3
creidhne <6192340+creidhne88@users.noreply.github.com>
2019-04-25 10:48:45 +0200

    set starting color to white

--- scripts/etcher.js
+++ scripts/etcher.js
@@ -21,24 +21,6 @@ etch.setup = function() {
 	etch.generatePad(16, etch.generateCont('etchpad', null, etch.container));
 };
 
-etch.generatePad = function(amount, parent) {
-	const size = Math.round((100 / amount) * 100) / 100;
-	for (let i = 0; i < amount ** 2; i++) {
-		let pixel = document.createElement('div');
-		pixel.classList.add('pixel');
-		pixel.style.backgroundColor = etch.getRandomColor();
-		pixel.style.width = `${size}%`;
-		pixel.style.height = `${size}%`;
-		parent.appendChild(pixel);
-		etch.pixels.push(pixel);
-	}
-	etch.pixels.map(el => {
-		el.addEventListener('mouseover', event => {
-			event.target.style.backgroundColor = etch.getRandomColor();
-		});
-	});
-};
-
 etch.generateCont = function(id, cssClass, parent) {
 	etch.containers[id] = document.createElement('div');
 	if (id) { etch.containers[id].setAttribute('id', id); }
@@ -52,6 +34,24 @@ etch.generateButton = function(text, parent) {
 	etch.buttons[text].innerHTML = text.charAt(0).toUpperCase() + text.slice(1);
 	parent.appendChild(etch.buttons[text]);
 	return etch.buttons[text];
+};
+
+etch.generatePad = function(amount, parent) {
+	const size = Math.round((100 / amount) * 100) / 100;
+	for (let i = 0; i < amount ** 2; i++) {
+		let pixel = document.createElement('div');
+		pixel.classList.add('pixel');
+		pixel.style.backgroundColor = '#ffffff';
+		pixel.style.width = `${size}%`;
+		pixel.style.height = `${size}%`;
+		parent.appendChild(pixel);
+		etch.pixels.push(pixel);
+	}
+	etch.pixels.map(el => {
+		el.addEventListener('mouseover', event => {
+			event.target.style.backgroundColor = etch.getRandomColor();
+		});
+	});
 };
 
 etch.clearPad = () => { etch.pixels.map(el => { el.style.backgroundColor = '#ffffff'; }); };
